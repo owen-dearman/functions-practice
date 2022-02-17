5f69c1c691395658c9a53a6f2c93fd2afb7fb67f
Owen Dearman <owendearman@gmail.com>
2022-02-17 16:31:25 +0000

    1c add pseudocode and tests

--- 1c-count-strings.js
+++ 1c-count-strings.js
@@ -8,6 +8,19 @@ Parameters:
 Returns:
 - STRING-COUNTS
     array of numbers
+=================================
 
+set STRING-COUNTS to an empty array
 
+for STRING in array STRINGS do
+    set STRING-LENGTH to length of STRING
+    add STRING-LENGTH to end of STRING-COUNTS
+endfor
+
+return STRING-COUNTS
+====================================
+Test Cases
+
+countStrings(["banana"]) // => [7]
+countStrings(["apple", "cheese", "lemonade"]) // => [5, 6, 8]
 */
