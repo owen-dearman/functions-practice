af49eaaaf896962b2e7c7cc4a7a4b5fefc145177
Owen Dearman <owendearman@gmail.com>
2022-02-17 15:41:02 +0000

    1a pseudocode

--- 1a-greetings.js
+++ 1a-greetings.js
@@ -7,4 +7,22 @@ Parameters:
 Returns:
 - GREETING-NAMES
     array of strings
+
+=========================
+set GREETING-NAMES to an empty array
+
+for NAME in array NAMES do
+    set GREETING-TEMPLATE to be a string "Hello, !"
+    splice NAME into GREETING-TEMPLATE at index 6.
+    add GREETING-TEMPLATE to the end of GREETING-NAMES
+endfor
+
+return GREETING-NAMES   
+
+
+==========================
+greetings(["Sam"]) // => ["Hello, Sam!"]
+greetings(["Bob", "Jimmy", "Sara"]) // => ["Hello, Bob!", "Hello, Jimmy!", "Hello, Sara!"]
+greetings([]) // => []
+
 */
