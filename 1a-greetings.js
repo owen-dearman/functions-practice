cb786b91dc4b4499c45447a790182dde0fde7e0e
Owen Dearman <owendearman@gmail.com>
2022-02-17 15:35:24 +0000

    initial commit 1a

---
+++ 1a-greetings.js
@@ -0,0 +1,10 @@
+//Function that takes and array of names and returns an array with a greeting for each of those names
+/*
+Function: greetings
+Parameters:
+- NAMES
+    array of strings
+Returns:
+- GREETING-NAMES
+    array of strings
+*/
