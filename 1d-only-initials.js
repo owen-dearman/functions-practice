1160e4133ee2c6d03900ab12e790e946b5783cf8
Owen Dearman <owendearman@gmail.com>
2022-02-17 16:43:16 +0000

    1d initial commit

---
+++ 1d-only-initials.js
@@ -0,0 +1,11 @@
+// Function that takes an array of names and returns an array of initials
+
+/*
+Function onlyInitials
+Parameters:
+- NAMES
+    array of strings
+Returns:
+-  INITIALS
+    array of strings
+*/
