1d87cccd07318664b9c6531331884aa8d00be758
Owen Dearman <owendearman@gmail.com>
2022-02-17 17:18:39 +0000

    initial commit 1e

---
+++ 1e-number-and-string.js
@@ -0,0 +1,21 @@
+"use strict"
+//Function that takes an array of numbers and returns an array of plain objects, with properties
+
+/*
+Function numberAndString
+Parameters:
+- NUMBERS 
+    array of numbers
+Returns:
+- NUMBER-AND-STRING
+    plain objects with
+            - asNumber
+                    the original number
+            - asString
+                    the string representation
+=============================================
+                
+
+
+
+*/
