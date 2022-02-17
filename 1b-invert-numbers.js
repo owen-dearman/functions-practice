7000dc4473de9ac31fb8f668fc4a1d502734b273
Owen Dearman <owendearman@gmail.com>
2022-02-17 16:03:12 +0000

    1b pseudocode parameters

---
+++ 1b-invert-numbers.js
@@ -0,0 +1,12 @@
+//Function to take an array of numbers and return an array with signs flipped
+
+/*
+Function invertNumbers
+Parameters:
+- NUMBERS
+    array of numbers
+Returns:
+- INVERTED-NUMBERS
+    array of numbers
+
+*/
