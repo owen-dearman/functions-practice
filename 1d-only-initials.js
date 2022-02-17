c57f32ca918f33b022af66939740704cdd52f5eb
Owen Dearman <owendearman@gmail.com>
2022-02-17 16:50:15 +0000

    1d pseudocode and tests added

--- 1d-only-initials.js
+++ 1d-only-initials.js
@@ -8,4 +8,23 @@ Parameters:
 Returns:
 -  INITIALS
     array of strings
+
+========================
+set INITIALS to an empty array
+
+for NAME of array NAMES do
+    set NAME-TO-INITIALS to a an array of NAME containing only the capital letters
+    set INDIVIDUAL-INITALS to a string of NAME-TO-INITIALS separated by "."
+    add a "." to the end of INDIVIDUAL-INITIALS
+    add INDIVIDUAL-INITIALS to the end of INITIALS
+endfor
+
+return INITIALS
+
+==============================
+
+onlyInitials(["Ben Johnson"]) // => ["B.J."]
+onlyInitials(["Adam Bowler", "Claire Devilson", "Edward Fox"]) // => ["A.B.", "C.D.", "E.F."]
+
+
 */
