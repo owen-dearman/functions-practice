b14e9cb792afab64bfd919988b14b1f17cc567e0
Owen Dearman <owendearman@gmail.com>
2022-02-17 16:27:56 +0000

    initial commit 1c

---
+++ 1c-count-strings.js
@@ -0,0 +1,13 @@
+//Function that takes an array of strings and returns an array with the length of each string
+
+/*
+Function countStrings
+Parameters:
+- STRINGS
+    array of strings
+Returns:
+- STRING-COUNTS
+    array of numbers
+
+
+*/
