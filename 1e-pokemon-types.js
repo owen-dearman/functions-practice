533e0610bd094cfac75b942ea71048da2beeb151
Owen Dearman <owendearman@gmail.com>
2022-02-18 09:03:36 +0000

    1e initial commit

---
+++ 1e-pokemon-types.js
@@ -0,0 +1,14 @@
+//Function that takes an object and returns a descriptive sentence about the Pokemon
+"use strict"
+
+/*
+Function pokemonTypes
+Parameters: 
+- POKEMON-INFORMATION
+    array of objects
+Returns:
+- POKEMON-DESCRIPTION
+    string
+
+
+*/
